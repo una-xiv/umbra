76dedd95bccd61d5d0e31214ac1584ed16998a7c
Harold Iedema <harold@iedema.me>
2024-08-12 12:25:55 +0200

    Add option to enable/disable DelvUI clip rects interop

--- Umbra/src/Umbra.DelvClipRects.cs
+++ Umbra/src/Umbra.DelvClipRects.cs
@@ -12,13 +12,23 @@ namespace Umbra;
 [Service]
 internal sealed class UmbraDelvClipRects : IDisposable
 {
+    [ConfigVariable("DelvUI.ClipRects.Enabled", "General", "WindowSettings")]
+    private static bool Enabled { get; set; } = true;
+
     private readonly HashSet<string> _managedRects = [];
 
     private readonly Dictionary<string, Vector4> _clipRects =
         Framework.DalamudPlugin.GetOrCreateData<Dictionary<string, Vector4>>("DelvUI.ClipRects", () => new());
 
+    public UmbraDelvClipRects()
+    {
+        ConfigManager.CvarChanged += OnCvarChanged;
+    }
+
     public void Dispose()
     {
+        ConfigManager.CvarChanged -= OnCvarChanged;
+
         foreach (var key in _managedRects) {
             _clipRects.Remove(key);
         }
@@ -31,6 +41,8 @@ internal sealed class UmbraDelvClipRects : IDisposable
     /// </summary>
     public void SetClipRect(string name, Vector4 rect)
     {
+        if (!Enabled) return;
+
         _clipRects[name] = rect;
         _managedRects.Add(name);
     }
@@ -49,6 +61,19 @@ internal sealed class UmbraDelvClipRects : IDisposable
     /// </summary>
     public void SetClipRect(string name, Vector2 position, Vector2 size)
     {
+        if (!Enabled) return;
+
         SetClipRect(name, new(position.X, position.Y, position.X + size.X, position.Y + size.Y));
+    }
+
+    private void OnCvarChanged(string name)
+    {
+        if (name != "DelvUI.ClipRects.Enabled") return;
+
+        if (!Enabled) {
+            foreach (var key in _managedRects) {
+                _clipRects.Remove(key);
+            }
+        }
     }
 }
