2e5571342efd0116a52d534d3c499f9f5f414192
Harold Iedema <harold@iedema.me>
2024-08-21 16:31:54 +0200

    Add a simple FPS widget

---
+++ Umbra/src/Toolbar/Widgets/Library/FpsCounter/FpsCounterWidget.cs
@@ -0,0 +1,40 @@
+﻿using FFXIVClientStructs.FFXIV.Client.System.Framework;
+using System.Collections.Generic;
+
+namespace Umbra.Widgets.Library.FPS;
+
+[ToolbarWidget("FpsCounter", "Widget.Fps.Name", "Widget.Fps.Description")]
+internal sealed class FpsCounterWidget(
+    WidgetInfo                  info,
+    string?                     guid         = null,
+    Dictionary<string, object>? configValues = null
+) : DefaultToolbarWidget(info, guid, configValues)
+{
+    /// <inheritdoc/>
+    public override WidgetPopup? Popup => null;
+
+
+    /// <inheritdoc/>
+    protected override IEnumerable<IWidgetConfigVariable> GetConfigVariables()
+    {
+        return [
+            ..DefaultToolbarWidgetConfigVariables,
+            ..SingleLabelTextOffsetVariables,
+        ];
+    }
+
+    /// <inheritdoc/>
+    protected override void Initialize()
+    {
+    }
+
+    protected override unsafe void OnUpdate()
+    {
+        int fps = (int)Framework.Instance()->FrameRate;
+
+        SetLabel($"{fps} FPS");
+        base.OnUpdate();
+
+        Node.Tooltip = $"{fps} FPS";
+    }
+}
