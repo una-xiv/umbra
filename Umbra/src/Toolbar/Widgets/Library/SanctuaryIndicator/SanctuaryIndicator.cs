c16e90294805669b7ec95bb5511f2ef07a4435bb
Harold Iedema <harold@iedema.me>
2024-08-21 21:51:02 +0200

    Add Sanctuary Indicator widget

---
+++ Umbra/src/Toolbar/Widgets/Library/SanctuaryIndicator/SanctuaryIndicator.cs
@@ -0,0 +1,38 @@
+﻿using Dalamud.Interface;
+using FFXIVClientStructs.FFXIV.Client.Game;
+using System.Collections.Generic;
+
+namespace Umbra.Widgets.Library.SanctuaryIndicator;
+
+[ToolbarWidget("SanctuaryIndicator", "Widget.SanctuaryIndicator.Name", "Widget.SanctuaryIndicator.Description")]
+internal sealed class SanctuaryIndicator(
+    WidgetInfo                  info,
+    string?                     guid         = null,
+    Dictionary<string, object>? configValues = null
+) : IconToolbarWidget(info, guid, configValues)
+{
+    /// <inheritdoc/>
+    public override WidgetPopup? Popup => null;
+
+    /// <inheritdoc/>
+    protected override IEnumerable<IWidgetConfigVariable> GetConfigVariables()
+    {
+        return [
+            ..DefaultIconToolbarWidgetConfigVariables,
+        ];
+    }
+
+    /// <inheritdoc/>
+    protected override void Initialize()
+    {
+        SetIcon(FontAwesomeIcon.Moon);
+    }
+
+    /// <inheritdoc/>
+    protected override void OnUpdate()
+    {
+        Node.Style.IsVisible = GameMain.IsInSanctuary();
+
+        base.OnUpdate();
+    }
+}
