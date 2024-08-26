4d209f71b8bb9e72d95b03fa0349fe01feb90679
Harold Iedema <harold@iedema.me>
2024-08-26 22:22:30 +0200

    Add Coordinates widget

---
+++ Umbra/src/Toolbar/Widgets/Library/Coordinates/CoordinatesWidget.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Numerics;
+using Umbra.Common;
+using Umbra.Game;
+
+namespace Umbra.Widgets.Library.Coordinates;
+
+[ToolbarWidget("Coordinates", "Widget.Coordinates.Name", "Widget.Coordinates.Description")]
+internal sealed class CoordinatesWidget(
+    WidgetInfo                  info,
+    string?                     guid         = null,
+    Dictionary<string, object>? configValues = null
+) : DefaultToolbarWidget(info, guid, configValues)
+{
+    public override WidgetPopup? Popup => null;
+
+    protected override void Initialize() { }
+
+    private IZoneManager ZoneManager { get; } = Framework.Service<IZoneManager>();
+
+    protected override IEnumerable<IWidgetConfigVariable> GetConfigVariables()
+    {
+        return [
+            ..DefaultToolbarWidgetConfigVariables,
+            ..SingleLabelTextOffsetVariables,
+        ];
+    }
+
+    protected override void OnUpdate()
+    {
+        if (!ZoneManager.HasCurrentZone) {
+            Node.Style.IsVisible = false;
+            return;
+        }
+
+        Node.Style.IsVisible = true;
+
+        Vector2 coords = ZoneManager.CurrentZone.PlayerCoordinates;
+        SetLabel($"{coords.X.ToString("0.0", CultureInfo.InvariantCulture)}, {coords.Y.ToString("0.0", CultureInfo.InvariantCulture)}");
+
+        base.OnUpdate();
+    }
+}
