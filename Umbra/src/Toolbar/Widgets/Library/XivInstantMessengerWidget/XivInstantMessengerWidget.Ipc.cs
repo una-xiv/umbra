fe211a5c2a4a87fe0a78d9289c60b7ddcf98b0bf
Harold Iedema <harold@iedema.me>
2024-12-01 16:34:45 +0100

    Add XIV Instant Messenger widget

---
+++ Umbra/src/Toolbar/Widgets/Library/XivInstantMessengerWidget/XivInstantMessengerWidget.Ipc.cs
@@ -0,0 +1,73 @@
+﻿using Dalamud.Plugin.Ipc;
+using System.Collections.Generic;
+using Umbra.Common;
+
+namespace Umbra.Widgets.Library.XIVInstantMessengerWidget;
+
+public sealed partial class XivInstantMessengerWidget
+{
+    private readonly ICallGateSubscriber<int>? _getConversationCount =
+        Framework.DalamudPlugin.GetIpcSubscriber<int>("Messenger.GetConversationCount");
+
+    private readonly ICallGateSubscriber<int>? _getUnreadConversationCount =
+        Framework.DalamudPlugin.GetIpcSubscriber<int>("Messenger.GetUnreadConversationCount");
+
+    private readonly ICallGateSubscriber<List<(string nameWithWorld, bool isUnread)>>? _getConversations =
+        Framework.DalamudPlugin.GetIpcSubscriber<List<(string nameWithWorld, bool isUnread)>>("Messenger.GetConversations");
+
+    private readonly ICallGateSubscriber<string, bool, bool>? _openMessenger =
+        Framework.DalamudPlugin.GetIpcSubscriber<string, bool, bool>("Messenger.OpenMessenger");
+
+    /// <summary>
+    /// Returns the number of loaded conversations.
+    /// </summary>
+    private int GetConversationCount()
+    {
+        try {
+            return _getConversationCount?.InvokeFunc() ?? 0;
+        } catch {
+            return 0;
+        }
+    }
+
+    /// <summary>
+    /// Returns the number of unread conversations.
+    /// </summary>
+    private int GetUnreadConversationCount()
+    {
+        try {
+            return _getUnreadConversationCount?.InvokeFunc() ?? 0;
+        } catch {
+            return 0;
+        }
+    }
+
+    /// <summary>
+    /// Returns a list of conversations.
+    /// </summary>
+    /// <remarks>
+    /// Relatively expensive operation, use sparingly.
+    /// </remarks>
+    private List<(string nameWithWorld, bool isUnread)> GetConversations()
+    {
+        try {
+            return _getConversations?.InvokeFunc() ?? [];
+        } catch {
+            return [];
+        }
+    }
+
+    /// <summary>
+    /// Opens the messenger window.
+    /// </summary>
+    /// <param name="nameWithWorld">Name of the recipient in the Name@World format.</param>
+    /// <param name="focusInputOnOpen">Whether to focus the input field on open.</param>
+    private void OpenMessenger(string nameWithWorld, bool focusInputOnOpen)
+    {
+        try {
+            _openMessenger?.InvokeAction(nameWithWorld, focusInputOnOpen);
+        } catch {
+            // ignored
+        }
+    }
+}
---
+++ Umbra/src/Toolbar/Widgets/Library/XivInstantMessengerWidget/XivInstantMessengerWidget.cs
@@ -0,0 +1,99 @@
+﻿using Dalamud.Plugin.Services;
+using System;
+using System.Collections.Generic;
+using Umbra.Common;
+using Una.Drawing;
+
+namespace Umbra.Widgets.Library.XIVInstantMessengerWidget;
+
+[InteropToolbarWidget(
+    "XIVInstantMessengerWidget",
+    "Widget.XivInstantMessenger.Name",
+    "Widget.XivInstantMessenger.Description",
+    "Messenger" // XIV Instant Messenger internal plugin name.
+)]
+public sealed partial class XivInstantMessengerWidget(
+    WidgetInfo                  info,
+    string?                     guid         = null,
+    Dictionary<string, object>? configValues = null
+) : DefaultToolbarWidget(info, guid, configValues)
+{
+    public override MenuPopup Popup { get; } = new();
+
+    protected override IEnumerable<IWidgetConfigVariable> GetConfigVariables()
+    {
+        return [
+            ..DefaultToolbarWidgetConfigVariables,
+            ..SingleLabelTextOffsetVariables
+        ];
+    }
+
+    /// <inheritdoc/>
+    protected override void Initialize()
+    {
+        SetIcon(63933);
+
+        Node.OnMouseDown  += OnMouseDown;
+        Node.OnRightClick += OpenXimWindow;
+    }
+
+    /// <inheritdoc/>
+    protected override void OnUpdate()
+    {
+        int count  = GetConversationCount();
+        int unread = GetUnreadConversationCount();
+
+        SetDisabled(count == 0);
+        SetLabel(unread > 0 ? I18N.Translate("Widget.XivInstantMessenger.UnreadCountLabel", unread) : null);
+
+        Popup.IsDisabled = count == 0;
+
+        base.OnUpdate();
+    }
+
+    /// <inheritdoc/>
+    protected override void OnDisposed()
+    {
+        Node.OnMouseDown  -= OnMouseDown;
+        Node.OnRightClick -= OpenXimWindow;
+
+        base.OnDisposed();
+    }
+
+    /// <summary>
+    /// Invoked when the user opens the popup.
+    /// </summary>
+    private void OnMouseDown(Node _)
+    {
+        Popup.Clear();
+
+        if (GetConversationCount() == 0) {
+            return;
+        }
+
+        List<(string name, bool isUnread)> conversations = GetConversations();
+        conversations.Sort((a, b) => String.Compare(a.name, b.name, StringComparison.OrdinalIgnoreCase));
+
+        int index = 0;
+
+        foreach (var (name, isUnread) in conversations) {
+            index++;
+
+            string[] nameParts = name.Split('@');
+
+            Popup.AddButton(
+                $"C_{index}",
+                label: nameParts[0],
+                altText: nameParts[1],
+                sortIndex: index,
+                iconId: isUnread ? (uint)63933 : null,
+                onClick: () => OpenMessenger(name, true)
+            );
+        }
+    }
+
+    private static void OpenXimWindow(Node _)
+    {
+        Framework.Service<ICommandManager>().ProcessCommand("/xim");
+    }
+}
