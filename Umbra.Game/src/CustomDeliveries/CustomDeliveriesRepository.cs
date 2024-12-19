f2ba20a52565d86649a2a3985cbed47cb31a764b
wolfcomp <4028289+wolfcomp@users.noreply.github.com>
2024-12-19 07:09:16 +0100

    improvements to display to include bonus

--- Umbra.Game/src/CustomDeliveries/CustomDeliveriesRepository.cs
+++ Umbra.Game/src/CustomDeliveries/CustomDeliveriesRepository.cs
@@ -1,4 +1,4 @@
-using Dalamud.Plugin.Services;
+﻿using Dalamud.Plugin.Services;
 using Dalamud.Utility;
 using FFXIVClientStructs.FFXIV.Client.Game;
 using FFXIVClientStructs.FFXIV.Client.Game.UI;
@@ -96,27 +96,38 @@ internal sealed unsafe class CustomDeliveriesRepository : ICustomDeliveriesRepos
 
         DeliveriesRemainingThisWeek = ssm->GetRemainingAllowances();
 
-        for (var i = 0; i < 10; i++) {
+        var adjustedTimestamp               = ssm->TimeAdjustmentForBonusGuarantee + FFXIVClientStructs.FFXIV.Client.System.Framework.Framework.GetServerTime();
+        var npcCount                        = ssm->SatisfactionRanks.Length;
+        var calculatedBonusGuaranteeRowId   = (uint)((adjustedTimestamp - 1657008000) / 604800 % npcCount);
+        var satisfactionBonusGuarentee      = DataManager.GetExcelSheet<SatisfactionBonusGuarantee>().GetRow(ssm->BonusGuaranteeRowId != 0xFF ? ssm->BonusGuaranteeRowId : calculatedBonusGuaranteeRowId);
+
+        for (var i = 0; i < npcCount; i++) {
             byte heartCount    = ssm->SatisfactionRanks[i]; // 1 ~ 5 (Hearts in the UI)
             byte usedAllowance = ssm->UsedAllowances[i];
+            int[] bonusType    = [];
+
+            if (heartCount == 5)
+                for (var j = 0; j < 2; j++) {
+                    if (satisfactionBonusGuarentee.BonusDoH[j] == i + 1)
+                        bonusType = [..bonusType, 5];
+                    if (satisfactionBonusGuarentee.BonusDoL[j] == i + 1)
+                        bonusType = [..bonusType, 6];
+                    if (satisfactionBonusGuarentee.BonusFisher[j] == i + 1)
+                        bonusType = [..bonusType, 7];
+                }
+
+            bool[] bonuses = [bonusType.Contains(5), bonusType.Contains(6), bonusType.Contains(7)];
 
             SatisfactionSupplyNpc? npc = SupplyNpcs.GetValueOrDefault(i + 1);
             if (npc == null) continue;
 
+            bonusType = [..bonusType, ..CalculateProbability(npc.SupplyIndices[heartCount], ssm->SupplySeed, bonuses)];
+            bonusType = bonusType.Distinct().Order().ToArray();
+
             if (!QuestManager.IsQuestComplete(npc.RequiredQuest)) continue;
 
             if (!Npcs.TryGetValue(i + 1, out CustomDeliveryNpc? customNpc)) {
-                var iconId = npc.IconId;
-
-                if (npc.IconId == 0) {
-                    // Not all NPCs have an icon, so we need to manually set it.
-                    iconId = (i + 1) switch {
-                        6  => 61666,
-                        9  => 61671,
-                        10 => 61673,
-                        _  => 0,
-                    };
-                }
+                var iconId = npc.IconIds[heartCount];
 
                 customNpc = new() {
                     Id                   = i + 1,
@@ -124,15 +135,74 @@ internal sealed unsafe class CustomDeliveriesRepository : ICustomDeliveriesRepos
                     IconId               = iconId,
                     HeartCount           = heartCount,
                     DeliveriesThisWeek   = usedAllowance,
-                    MaxDeliveriesPerWeek = npc.MaxDeliveriesPerWeek
+                    MaxDeliveriesPerWeek = npc.MaxDeliveriesPerWeek,
+                    BonusType            = bonusType
                 };
 
                 Npcs.Add(i + 1, customNpc);
             } else {
                 customNpc.HeartCount         = heartCount;
                 customNpc.DeliveriesThisWeek = usedAllowance;
+                customNpc.BonusType          = bonusType;
             }
         }
+    }
+
+    private int[] CalculateProbability(uint npcId, uint seed, bool[] bonuses)
+    {
+        var subRows = DataManager.GetSubrowExcelSheet<SatisfactionSupply>().GetRow(npcId);
+
+        var h1 = (0x03CEA65Cu * npcId) ^ (0x1A0DD20Eu * seed);
+        var h2 = (0xDF585D5Du * npcId) ^ (0x3057656Eu * seed);
+        var h3 = (0xED69E442u * npcId) ^ (0x2202EA5Au * seed);
+        var h4 = (0xAEFC3901u * npcId) ^ (0xE70723F6u * seed);
+        int[] res = [-1, -1, -1];
+        var h5 = h1;
+        for (int iSlot = 1; iSlot < 4; ++iSlot)
+        {
+            var sumProbabilities = subRows
+                .Where(row => row.Slot == iSlot)
+                .Aggregate(0, (current, row) => current + row.ProbabilityPercent);
+
+            var hTemp = h5 ^ (h5 << 11);
+            h1 = h3;
+            h3 = h4;
+            h5 = h2;
+            h4 ^= hTemp ^ ((hTemp ^ (h4 >> 11)) >> 8);
+            h2 = h1;
+
+            var roll = h4 % sumProbabilities;
+            for (int iSub = 0; iSub < subRows.Count; ++iSub)
+            {
+                var row = subRows[iSub];
+                if (row.Slot != iSlot)
+                    continue;
+                if (roll < row.ProbabilityPercent)
+                {
+                    res[iSlot - 1] = iSub;
+                    break;
+                }
+                roll -= row.ProbabilityPercent;
+            }
+        }
+
+        int[] bonusEffective = [];
+
+        for (var i = 0; i < res.Length; i++) {
+            var supply = subRows[res[i]];
+            if (!supply.IsBonus && bonuses[i]) {
+                foreach (var supplyOverride in subRows.Where(supplyOverride =>
+                             supplyOverride.Slot == supply.Slot && supplyOverride.IsBonus)) {
+                    supply = supplyOverride;
+                    break;
+                }
+            }
+
+            if (supply.IsBonus)
+                bonusEffective = [..bonusEffective, supply.IsBonus ? i + 5 : -1];
+        }
+
+        return bonusEffective.Where(i => i != -1).ToArray();
     }
 
     private void HydrateStaticData()
@@ -144,9 +214,10 @@ internal sealed unsafe class CustomDeliveriesRepository : ICustomDeliveriesRepos
                 (int)s.RowId,
                 new(
                     s.QuestRequired.RowId,
-                    (uint)s.Icon,
+                    s.RankParams.Select(t => (uint)t.ImageId).ToArray(),
                     s.DeliveriesPerWeek,
-                    s.Npc.Value.Singular.ExtractText()
+                    s.Npc.Value.Singular.ExtractText(),
+                    s.SatisfactionNpcParams.Select(t => (uint)t.SupplyIndex).ToArray()
                 )
             );
         }
@@ -154,8 +225,9 @@ internal sealed unsafe class CustomDeliveriesRepository : ICustomDeliveriesRepos
 
     private record SatisfactionSupplyNpc(
         uint   RequiredQuest,
-        uint   IconId,
+        uint[] IconIds,
         byte   MaxDeliveriesPerWeek,
-        string Name
+        string Name,
+        uint[] SupplyIndices
     );
 }
