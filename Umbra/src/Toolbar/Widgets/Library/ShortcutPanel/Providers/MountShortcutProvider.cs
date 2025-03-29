fc25e0e18b38d235e168f9081e816e240ab456e0
Haselnussbomber <mail@haselnussbomber.de>
2025-03-29 19:23:37 +0100

    Remove unnecessary using

--- Umbra/src/Toolbar/Widgets/Library/ShortcutPanel/Providers/MountShortcutProvider.cs
+++ Umbra/src/Toolbar/Widgets/Library/ShortcutPanel/Providers/MountShortcutProvider.cs
@@ -7,7 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Umbra.Common;
-using static Dalamud.Interface.Utility.Raii.ImRaii;
 
 namespace Umbra.Widgets.Library.ShortcutPanel.Providers;
 
