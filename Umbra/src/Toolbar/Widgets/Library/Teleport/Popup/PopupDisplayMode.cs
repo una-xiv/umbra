33050a869f0d0f9e22bbb5e05341050134cb0264
Harold Iedema <harold@iedema.me>
2025-04-27 15:43:52 +0200

    Add missing translations for the DisplayMode option of the TeleportWidget

--- Umbra/src/Toolbar/Widgets/Library/Teleport/Popup/PopupDisplayMode.cs
+++ Umbra/src/Toolbar/Widgets/Library/Teleport/Popup/PopupDisplayMode.cs
@@ -1,7 +1,12 @@
-﻿namespace Umbra.Widgets.Popup;
+﻿using Umbra.Common;
+
+namespace Umbra.Widgets.Popup;
 
 public enum PopupDisplayMode
 {
+    [TranslationKey("Widget.Teleport.Config.PopupDisplayMode.Option.Extended")]
     Extended  = 0,
+    
+    [TranslationKey("Widget.Teleport.Config.PopupDisplayMode.Option.Condensed")]
     Condensed = 1,
 }
