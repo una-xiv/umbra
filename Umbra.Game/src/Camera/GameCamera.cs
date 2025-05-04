66efb932b43f2f8e2e1ae7af5241a76ab38eed5d
Harold Iedema <harold@iedema.me>
2025-05-04 16:03:04 +0200

    Fix GameCamera - do not use cached angles

--- Umbra.Game/src/Camera/GameCamera.cs
+++ Umbra.Game/src/Camera/GameCamera.cs
@@ -9,27 +9,14 @@ namespace Umbra.Game;
 [Service]
 internal sealed class GameCamera : IGameCamera
 {
-    private Vector3 _cameraPosition;
-    private Vector3 _lookAtVector;
-
     public unsafe bool WorldToScreen(
         Vector3 worldPos, out Vector2 screenPos, int horizontalOffset = 0, int verticalOffset = 0
     )
     {
-        CameraManager* cmPtr = CameraManager.Instance();
-
-        if (cmPtr == null) {
-            screenPos = new();
-            return false;
-        }
-
         FFXIVClientStructs.FFXIV.Common.Math.Vector2 v2 = new();
         FFXIVClientStructs.FFXIV.Common.Math.Vector3 v3 = new(worldPos.X, worldPos.Y, worldPos.Z);
         Camera.WorldToScreenPoint(&v2, &v3);
         screenPos = new(v2.X, v2.Y);
-
-        _lookAtVector   = cmPtr->CurrentCamera->LookAtVector;
-        _cameraPosition = cmPtr->CurrentCamera->Position;
 
         bool isInViewport = screenPos.X >= horizontalOffset
             && screenPos.X <= ImGui.GetIO().DisplaySize.X - horizontalOffset
@@ -41,21 +28,33 @@ internal sealed class GameCamera : IGameCamera
 
     public bool IsInFrontOfCamera(Vector3 worldPos)
     {
-        Vector3 cameraDirection = Vector3.Normalize(_lookAtVector - _cameraPosition);
-        Vector3 toPoint         = worldPos - _cameraPosition;
+        (Vector3 lookAtVector, Vector3 cameraPosition) = GetCameraVectors();
+        
+        Vector3 cameraDirection = Vector3.Normalize(lookAtVector - cameraPosition);
+        Vector3 toPoint         = worldPos - cameraPosition;
 
         return Vector3.Dot(cameraDirection, toPoint) > 0;
     }
     
     public float GetCameraAngle()
     {
-        Vector3 cameraDirection = Vector3.Normalize(_lookAtVector - _cameraPosition);
-        var result = MathF.Atan2(cameraDirection.Z, cameraDirection.X);
+        (Vector3 lookAtVector, Vector3 cameraPosition) = GetCameraVectors();
+        
+        Vector3 cameraDirection = Vector3.Normalize(lookAtVector - cameraPosition);
+        var     result          = MathF.Atan2(cameraDirection.Z, cameraDirection.X);
         
         if (float.IsNaN(result)) {
             return 0;
         }
 
         return result;
+    }
+
+    private unsafe (Vector3 position, Vector3 lookAt) GetCameraVectors()
+    {
+        CameraManager* cmPtr = CameraManager.Instance();
+        return cmPtr == null 
+            ? (default, default) 
+            : (cmPtr->CurrentCamera->LookAtVector, cmPtr->CurrentCamera->Position);
     }
 }
