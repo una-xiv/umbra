8fd94abecaa05da4676eda39b4cadde5f579ce7e
Harold Iedema <harold@iedema.me>
2025-08-25 01:10:07 +0200

    Add Alarms widget (quick-access only, game data unavailable right now)

---
+++ Umbra/src/Toolbar/Widgets/Library/Alarm/AlarmWidget.cs
@@ -0,0 +1,28 @@
+﻿using Dalamud.Game.Text.SeStringHandling;
+
+namespace Umbra.Widgets.Library.Alarm;
+
+[ToolbarWidget(
+    "Alarm",
+    "Widget.Alarm.Name",
+    "Widget.Alarm.Description",
+    ["alarm", "reminder", "timer", "clock"]
+)]
+public sealed class AlarmWidget(
+    WidgetInfo                  info,
+    string?                     guid         = null,
+    Dictionary<string, object>? configValues = null
+) : StandardToolbarWidget(info, guid, configValues)
+{
+    protected override StandardWidgetFeatures Features =>
+        StandardWidgetFeatures.Icon |
+        StandardWidgetFeatures.CustomizableIcon;
+
+    protected override string         DefaultIconType   => IconTypeBitmapIcon;
+    protected override BitmapFontIcon DefaultBitmapIcon => BitmapFontIcon.Alarm;
+
+    protected override void OnLoad()
+    {
+        Node.OnClick += _ => Framework.Service<IChatSender>().Send("/alarm");
+    }
+}
