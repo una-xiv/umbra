33410c00551b1fe403d44a942ef9442e60b7648d
Jerric <58122205+Ivinedra@users.noreply.github.com>
2026-01-01 20:13:01 -0500

    Dynamic Menu Categories
    Adds a "category" option to the Dynamic Menu.
    
    Categories act like folders on a computer, with the notable difference that when a category is deleted - all items are placed as parents.

---
+++ Umbra/src/Toolbar/Widgets/Library/DynamicMenu/DynamicMenuPopup.CategoryEditor.cs
@@ -0,0 +1,35 @@
+﻿using Umbra.Windows;
+using Umbra.Windows.Library.VariableEditor;
+
+namespace Umbra.Widgets;
+
+internal sealed partial class DynamicMenuPopup
+{
+    private void OpenCategoryEditor()
+    {
+        if (null == _selectedItemIndex) return;
+        DynamicMenuEntry entry = Entries[_selectedItemIndex.Value];
+
+        StringVariable labelVar = new("Label") {
+            Name = I18N.Translate("Widget.DynamicMenu.CategoryEditor.Label.Name"),
+            Description = I18N.Translate("Widget.DynamicMenu.CategoryEditor.Label.Description"),
+            Value = entry.Cl ?? "",
+            MaxLength = 100,
+        };
+
+        List<Variable> variables = [labelVar];
+        VariablesEditorWindow window = new(I18N.Translate("Widget.DynamicMenu.CategoryEditor.Title"), variables, []);
+
+        Framework
+            .Service<WindowManager>()
+            .Present(
+                "CategoryEditor",
+                window,
+                _ => {
+                    entry.Cl = labelVar.Value;
+                    RebuildMenu();
+                    OnEntriesChanged?.Invoke();
+                }
+            );
+    }
+}
