43a7d44b120c2582208a5aadf8aa38b6a0273ab4
Bloodsoul <bloodsoul@dragon-force.org>
2026-03-27 19:38:41 +0100

    autohide for aux

--- Umbra/src/Toolbar/Toolbar.Nodes.cs
+++ Umbra/src/Toolbar/Toolbar.Nodes.cs
@@ -63,7 +63,28 @@ internal partial class Toolbar
                 _        => config.YPos,
             };
 
-            auxBarNode.Render(drawList, new(xPos, yPos));
+            // Apply autohide effects for this auxbar if enabled
+            float auxYOffset = 0;
+            float auxOpacity = 1.0f;
+
+            if (config.EnableAutoHide && ShouldAutoHide()) {
+                auxYOffset = config.YAlign switch {
+                    "Top"    => _autoHideYOffset,
+                    "Bottom" => -_autoHideYOffset,
+                    _        => 0,
+                };
+
+                if (IsMultiMonitorSupportEnabled()) {
+                    auxOpacity = _autoHideOpacity;
+                    auxYOffset = 0;
+                } else {
+                    auxOpacity = _autoHideOpacity;
+                }
+            }
+
+            auxBarNode.Style.Opacity = auxOpacity;
+
+            auxBarNode.Render(drawList, new(xPos, yPos + auxYOffset));
         }
     }
 
