ca3e5f14a32527f706285b2f4e6eff8a64292030
Bloodsoul <Bloodsoul@users.noreply.github.com>
2026-07-19 11:44:18 +0200

    Add placeholders for free company name and tag (#471)

---
+++ Umbra.Game/src/Script/Placeholders/Library/FreeCompanyNamePlaceholder.cs
@@ -0,0 +1,17 @@
+﻿using FFXIVClientStructs.FFXIV.Client.UI.Info;
+
+namespace Umbra.Game.Script;
+
+[Service]
+internal class FreeCompanyNamePlaceholder() : ScriptPlaceholder(
+    "fc.name",
+    "The name of the free company the player is a member of."
+)
+{
+    [OnTick]
+    public unsafe void Update()
+    {
+        var fc = InfoProxyFreeCompany.Instance();
+        Value = fc != null ? fc->NameString : "";
+    }
+}
