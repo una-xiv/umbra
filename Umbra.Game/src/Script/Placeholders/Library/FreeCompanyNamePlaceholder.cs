6146eed30d27100f28c629f645a21aac432a7f61
Bloodsoul <Bloodsoul@users.noreply.github.com>
2026-07-30 06:16:10 +0200

    Only update fc placeholders while receiving non-empty data (#473)

--- Umbra.Game/src/Script/Placeholders/Library/FreeCompanyNamePlaceholder.cs
+++ Umbra.Game/src/Script/Placeholders/Library/FreeCompanyNamePlaceholder.cs
@@ -1,4 +1,5 @@
-﻿using FFXIVClientStructs.FFXIV.Client.UI.Info;
+﻿using Dalamud.Utility;
+using FFXIVClientStructs.FFXIV.Client.UI.Info;
 
 namespace Umbra.Game.Script;
 
@@ -12,6 +13,15 @@ internal class FreeCompanyNamePlaceholder() : ScriptPlaceholder(
     public unsafe void Update()
     {
         var fc = InfoProxyFreeCompany.Instance();
-        Value = fc != null ? fc->NameString : "";
+
+        // While world visiting or during duty, fc info is not available.
+        // Currently, the NameString property also does not update when returning from a duty.
+        // This also keeps the placeholder from being updated when the player leaves their fc.
+        if (fc == null || fc->Id == 0 || fc->NameString.IsNullOrEmpty())
+        {
+            return;
+        }
+
+        Value = fc->NameString;
     }
 }
